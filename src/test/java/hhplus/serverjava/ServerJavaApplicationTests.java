272cd3c7e6f948321f3beab16acf562f6311d57c
corncode8 <myeongcheol8@naver.com>
2024-04-01 16:04:09 +0900

    initial commit

---
+++ src/test/java/hhplus/serverjava/ServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package hhplus.serverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
