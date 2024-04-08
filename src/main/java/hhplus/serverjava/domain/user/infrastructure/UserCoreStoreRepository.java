124a2cef1c5e6897a7be211a2ac7e924a0005cfb
corncode8 <myeongcheol8@naver.com>
2024-04-08 15:06:23 +0900

    Scheduler 구현

--- src/main/java/hhplus/serverjava/domain/user/infrastructure/UserCoreStoreRepository.java
+++ src/main/java/hhplus/serverjava/domain/user/infrastructure/UserCoreStoreRepository.java
@@ -1,12 +1,10 @@
 package hhplus.serverjava.domain.user.infrastructure;
 
-import hhplus.serverjava.api.util.exceptions.BaseException;
 import hhplus.serverjava.domain.user.entity.User;
 import hhplus.serverjava.domain.user.repository.UserStoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static hhplus.serverjava.api.util.response.BaseResponseStatus.NOT_FIND_USER;
 
 @Repository
 @RequiredArgsConstructor
